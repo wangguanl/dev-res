47ea9cdafc3095692be69aeb677b273517858021
Mr.Wang <1285454533@qq.com>
2026-03-14 23:35:41 +0800

    feat: add .gitignore, configure backend and frontend environment, and implement basic server functionality

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Home from '../pages/Home.vue';
 import Preview from '../pages/Preview.vue';
@@ -11,7 +11,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
